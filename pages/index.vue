c39a168ac479f093be4b0f1df28982ecaf54d6de
Pitt Wu <kgb00128@gmail.com>
2020-03-17 13:31:23 +0800

    init new project

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .container init
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ plugins/vue-material.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import vueMaterial from 'vue-material'
+
+Vue.use(vueMaterial)
